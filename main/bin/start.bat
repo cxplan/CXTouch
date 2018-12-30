3366660a7dc6af4f919fc3e4c36ccb6ff311ceae
kennyliu <asdfxcse@234ll23_>
2018-12-30 15:22:21 +0800

    Revise package files.

--- main/bin/start.bat
+++ main/bin/start.bat
@@ -1,13 +1,9 @@
 @echo off
-set  CURRENT=%cd%
 cd ..
-SET CLIENT_HOME=%cd%
 
-set CONFPATH=%CLIENT_HOME%\conf\
-set LIB_PATH=%CLIENT_HOME%\lib
-set RUNJAR=%CLIENT_HOME%/lib/${artifactId}-${version}.jar
-rem some properties which need by app
-set PROPERTIES_LOG4J_LOGPATH=log4j.logPath
+set CONFPATH=conf/
+set LIB_PATH=lib
+set RUNJAR=lib/${artifactId}-${version}.jar
 rem run!
 @echo on
 @java -Xbootclasspath/a:%CONFPATH% -Xmx256m -Xms100m -Dsun.java2d.opengl=False -Dsun.java2d.xrender=True -Dconfig.path=%CONFPATH% -jar %RUNJAR%
--- main/bin/start.sh
+++ main/bin/start.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-CURRENT=`pwd`
 cd ..
-CLIENT_HOME=`pwd`
 
-CONFPATH=$CLIENT_HOME/conf
-RUNJAR=$CLIENT_HOME/lib/${artifactId}-${version}.jar
-##some properties needed by app
-PROPERTIES_LOG4J_LOGPATH=log4j.logPath
+CONFPATH=conf
+RUNJAR=lib/${artifactId}-${version}.jar
 ## run!
 java -Xbootclasspath/a:$CONFPATH -Xmx256m -Xms100m -Dsun.java2d.opengl=False -Dsun.java2d.xrender=True -Dconfig.path=$CONFPATH -jar $RUNJAR
